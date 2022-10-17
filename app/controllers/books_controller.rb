4f1821476364469cf8bc92bb8c406ce1f748997d
EC2 Default User <ec2-user@ip-172-31-29-187.ap-northeast-1.compute.internal>
2022-10-17 08:53:26 +0000

    [Add] 応用課題3 コメント機能のフォーム

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
     @books = Book.new
     @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
-
+    @book_comment = BookComment.new
   end
 
   def index
