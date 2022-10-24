9e5533e007436e8db2a7c03fc4f63e90ddf46733
EC2 Default User <ec2-user@ip-172-31-29-187.ap-northeast-1.compute.internal>
2022-10-24 02:42:13 +0000

    [Add] フォロー機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :followers, class_name: 'relationship', foreign_key: 'follower_id',dependent: :de
 
   has_one_attached :profile_image
 
