8c2946c0449c855874e8a75227b846caab48f344
EC2 Default User <ec2-user@ip-172-31-29-187.ap-northeast-1.compute.internal>
2022-11-10 06:12:54 +0000

    [Finish] 応用課題9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book_comment = BookComment.new
+    current_user.view_counts.create(book_id: @book.id)
   end
 
   def index
